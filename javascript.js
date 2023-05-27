224298c8c30135a10d0d63008f4c925193938ab4
Dani <dmmraymundo1@gmail.com>
2023-05-27 09:03:04 +0800

    Modify playRound

--- javascript.js
+++ javascript.js
@@ -14,37 +14,11 @@ let playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1)
 // get computer's choice
 let computerSelection = getComputerChoice(choices);
 
-// play 1 round
-// function playRound (playerSelection, computerSelection) {
-//     if (choices.includes(playerSelection)) {
-//         if (playerSelection === computerSelection) {
-//             return alert(`You chose ${playerSelection}, Computer chose ${computerSelection}. It's a tie!`);
-//         } else if (playerSelection === "Rock") {
-//             if (computerSelection === "Paper") {
-//                 return alert(`You chose ${playerSelection}, Computer chose ${computerSelection}. Paper beats rock. You lose.`);
-//             } else {
-//                 return alert(`You chose ${playerSelection}, Computer chose ${computerSelection}. Rock beats scissors. You win!`); 
-//             };
-//         } else if (playerSelection === "Paper") {
-//             if (computerSelection === "Rock") {
-//                 return alert(`You chose ${playerSelection}, Computer chose ${computerSelection}. Paper beats rock. You win!`);
-//             } else {
-//                 return alert(`You chose ${playerSelection}, Computer chose ${computerSelection}. Scissors beat paper. You lose.`);
-//             };
-//         } else if (playerSelection === "Scissors") {
-//             if (computerSelection === "Rock") {
-//                 return alert(`You chose ${playerSelection}, Computer chose ${computerSelection}. Rock beats scissors. You lose.`);
-//             } else {
-//                 return alert(`You chose ${playerSelection}, Computer chose ${computerSelection}. Scissors beat paper. You win!`);
-//             }
-//         };
-//     } else {
-//         return alert("Invalid choice")
-//     }
-// };
+// start the game with 0 as both scores
 let playerScore = 0;
 let computerScore = 0;
 
+// play one round
 function playRound (playerSelection, computerSelection) {
     if (choices.includes(playerSelection)) {
         if (playerSelection === computerSelection) {
