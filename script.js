05515ea2fc2603a2d58f93200ec1f61d148efc62
Dani <dmmraymundo1@gmail.com>
2023-06-05 08:27:26 +0800

    Display selection when buttons are clicked

--- script.js
+++ script.js
@@ -4,79 +4,121 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-    // A flag variable is used to track if the game was terminated prematurely due to an invalid user input
-    let gameTerminated = false;
+    // // A flag variable is used to track if the game was terminated prematurely due to an invalid user input
+    // let gameTerminated = false;
 
-    
-    for (let round = 1; round <=5; round++) {
+    const rockButton = document.querySelector("#choice-rock");
+    const paperButton = document.querySelector("#choice-paper");
+    const scissorsButton = document.querySelector("#choice-scissors");
+    const playerSelection = document.querySelector("#player-selection");
+    const computerSelection = document.querySelector("#computer-selection");
 
-        
-        console.log(`Round ${round}`);
-
-        
-        function getComputerChoice(choices) {
-            const randomIndex = Math.floor(Math.random() * choices.length);
-            return choices[randomIndex];
-        };
-
-        
-        let playerInput = prompt("Up for a game of Rock Paper Scissors? Enter your choice.");
-        let playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
-
-        
-        if (!choices.includes(playerSelection)) {
-            console.log("Invalid choice. Exiting the game. Refresh to start a new one.")
-            gameTerminated = true;
-            break;
-        };
-
-        
-        let computerSelection = getComputerChoice(choices);
-
-        
-        function playRound (playerSelection, computerSelection) {
-            if (playerSelection === computerSelection) {
-                console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}. It's a tie.`)
-            } else if (
-                (playerSelection === "Rock") && (computerSelection === "Scissors") ||
-                (playerSelection === "Paper") && (computerSelection === "Rock") ||
-                (playerSelection === "Scissors") && (computerSelection === "Paper")
-            ) {
-                console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}. You get a point.`)
-                return "player";
-            } else {
-                console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}. Computer gets a point.`)
-                return "computer";
-            };
-        };
-        
-        let result = playRound(playerSelection, computerSelection);
-
-        
-        if (result === "player") {
-            playerScore++;
-        } else if (result === "computer") {
-            computerScore++;
-        };
-
-        
-        console.log(`Your score: ${playerScore}.\nComputer's score: ${computerScore}`);
-
+    function getComputerChoice(choices) {
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
     };
 
-    // The final result is only displayed when all 5 rounds have been completed, i.e, the game wasn't stopped due to an invalid user input.
-    if (!gameTerminated) {
-        if (playerScore > computerScore) {
-            alert("Game over. You win!");
-            alert("Refresh to play another one.");
-        } else if (playerScore < computerScore) {
-            alert("Game over. You lose.")
-            alert("Refresh to play another one.");
+    rockButton.addEventListener("click", () => {
+        displayPlayerSelection("rock");
+        displayComputerSelection(getComputerChoice(choices));
+    });
+
+    paperButton.addEventListener("click", () => {
+        displayPlayerSelection("paper");
+        displayComputerSelection(getComputerChoice(choices));
+    });
+
+    scissorsButton.addEventListener("click", () => {
+        displayPlayerSelection("scissors");
+        displayComputerSelection(getComputerChoice(choices));
+    });
+
+    function displayPlayerSelection(choice) {
+        if (choice === "rock") {
+            playerSelection.src ="rps-images/player-rock.png";
+        } else if (choice === "paper") {
+            playerSelection.src ="rps-images/player-paper.png";
         } else {
-            alert("Game over. It's a tie.")
-            alert("Refresh to play another one.");
+            playerSelection.src ="rps-images/player-scissors.png";
         }
     };
+
+    function displayComputerSelection(choice) {
+        if (getComputerChoice(choices) === "Rock") {
+            computerSelection.src ="rps-images/comp-rock.png";
+        } else if (getComputerChoice(choices) === "Paper") {
+            computerSelection.src ="rps-images/comp-paper.png";
+        } else {
+            computerSelection.src ="rps-images/comp-scissors.png";
+        }
+    };
+    
 };
+//     for (let round = 1; round <=5; round++) {
+
+        
+//         console.log(`Round ${round}`);
+
+        
+       
+        
+//         let playerInput = prompt("Up for a game of Rock Paper Scissors? Enter your choice.");
+//         let playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
+
+        
+//         if (!choices.includes(playerSelection)) {
+//             console.log("Invalid choice. Exiting the game. Refresh to start a new one.")
+//             gameTerminated = true;
+//             break;
+//         };
+
+        
+        
+
+        
+//         function playRound (playerSelection, computerSelection) {
+//             if (playerSelection === computerSelection) {
+//                 console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}. It's a tie.`)
+//             } else if (
+//                 (playerSelection === "Rock") && (computerSelection === "Scissors") ||
+//                 (playerSelection === "Paper") && (computerSelection === "Rock") ||
+//                 (playerSelection === "Scissors") && (computerSelection === "Paper")
+//             ) {
+//                 console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}. You get a point.`)
+//                 return "player";
+//             } else {
+//                 console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}. Computer gets a point.`)
+//                 return "computer";
+//             };
+//         };
+        
+//         let result = playRound(playerSelection, computerSelection);
+
+        
+//         if (result === "player") {
+//             playerScore++;
+//         } else if (result === "computer") {
+//             computerScore++;
+//         };
+
+        
+//         console.log(`Your score: ${playerScore}.\nComputer's score: ${computerScore}`);
+
+//     };
+
+//     // The final result is only displayed when all 5 rounds have been completed, i.e, the game wasn't stopped due to an invalid user input.
+//     if (!gameTerminated) {
+//         if (playerScore > computerScore) {
+//             alert("Game over. You win!");
+//             alert("Refresh to play another one.");
+//         } else if (playerScore < computerScore) {
+//             alert("Game over. You lose.")
+//             alert("Refresh to play another one.");
+//         } else {
+//             alert("Game over. It's a tie.")
+//             alert("Refresh to play another one.");
+//         }
+//     };
+// };
 
 game();
